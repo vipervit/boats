fa1d094b4faf02d751ad0f248311fe669d2a922b
Viper Vit <vitolg1@gmail.com>
2023-01-03 16:52:52 -0500

    Update petsamo.py
    Remove ranking.

--- petsamo.py
+++ petsamo.py
@@ -10,8 +10,7 @@ from pathlib import Path
 
 f_name='petsamo.gpx'
 
-res_gen = requests.get('http://srv.sailaway.world/cgi-bin/sailaway/APIBoatInfo.pl?usrnr=59528&key=7B79EE2988A44080A37C06570F4B5EE8')
-res_rank=requests.get('https://sarl.ingenium.net.au/sarl?racenr=38602')
+response = requests.get('http://srv.sailaway.world/cgi-bin/sailaway/APIBoatInfo.pl?usrnr=59528&key=7B79EE2988A44080A37C06570F4B5EE8')
 
 platform=sys.platform
 
@@ -24,9 +23,7 @@ else:
 
 f_pos=os.path.join(root, 'Boats', 'Petsamo', f_name)
 
-rank=res_rank.text.split('Ketch | Petsamo')[0][-18:].split(' ')[0].replace('>', '')
-
-for boat in res_gen.json():
+for boat in response.json():
     name=boat['boatname']
     if name=='Petsamo':
         lat=round(boat['latitude'],3)
@@ -39,8 +36,6 @@ for boat in res_gen.json():
         print('twd: {}'.format(round(boat['twd'])))
         print('twa: {}'.format(round(boat['twa'])))
         print('heel: {}'.format(round(boat['heeldegrees'])))
-        print('rank: {}'.format(rank))
-
 
 s='<?xml version="1.0"?>\n<gpx>\n   <wpt lat="LATITUDE" lon="LONGTITUDE">\n      <name>TIME</name>\n      <desc>track position</desc>\n   </wpt>\n</gpx>'
 s=s.replace('LATITUDE', str(lat))
