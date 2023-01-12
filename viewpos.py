71d78633071ada208782146927322dc29c554644
vipervit <vitolg1@gmail.com>
2023-01-12 14:05:57 -0500

    Add zoom in viewpos.py

--- viewpos.py
+++ viewpos.py
@@ -8,15 +8,17 @@ from lib.boat import boat
 drv = webdriver.Firefox()
 
 sleeptime = int(sys.argv[2])
+zoom = str(sys.argv[3])
 
 while True:
-    url = 'https://fishing-app.gpsnauticalcharts.com/i-boating-fishing-web-app/fishing-marine-charts-navigation.html#9/LAT/LON'
+    url = 'https://fishing-app.gpsnauticalcharts.com/i-boating-fishing-web-app/fishing-marine-charts-navigation.html#ZOOM/LAT/LON'
     oBoat = boat(sys.argv[1])
     oBoat.getdata()
     pos = oBoat.pos
     del oBoat
     url = url.replace('LAT', str(round(pos[0],4)))
     url = url.replace('LON', str(round(pos[1],4)))
+    url = url.replace('ZOOM', zoom)
     drv.get(url)
     if sleeptime == 0:
         break
