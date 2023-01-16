6a8ba9eac2bf33b70b8cc8d76a934814acf001d4
Viper Vit <vitolg1@gmail.com>
2023-01-16 18:59:31 -0500

    Improve racemap.py with Boatmarker

--- racemap.py
+++ racemap.py
@@ -8,36 +8,38 @@ from datetime import datetime
 
 import folium
 import pandas as pd
-import requests
-from selenium import webdriver
+from folium.plugins import BoatMarker
 
-from lib.common import API_RACES, timeago, DIR_HTML
+from lib.common import timeago, DIR_HTML, get_race_data
 
 boat = sys.argv[1]
 race = sys.argv[2]
 launch_browser = sys.argv[3]
-url_race = API_RACES[race]
 html = os.path.join(DIR_HTML, '{}.html'.format(race))
 
-res=requests.get(url_race)
-boats = res.json()['result']
-data=[(boats[boat]['ubtname'],
-       boats[boat]['lat_dec'],
-       boats[boat]['lon_dec'],
-       boats[boat]['rank'],
-       boats[boat]['track'],
-       int(boats[boat]['timestamp']/1000))
-      for boat in list(boats.keys()) if 'lat_dec' in boats[boat]]
+boats = get_race_data(race)
 
-df=pd.DataFrame(data, columns=['Boat', 'Lat', 'Lon', 'Rank', 'Track', 'Timestamp'])
-df['Rank']=df['Rank'].astype('int64')
+data = [(boats[boat]['ubtname'],
+         boats[boat]['lat_dec'],
+         boats[boat]['lon_dec'],
+         boats[boat]['rank'],
+         boats[boat]['track'],
+         int(boats[boat]['timestamp'] / 1000),
+         boats[boat]['heading'],
+         boats[boat]['lastreport_speed'],
+         boats[boat]['wind'].split(',')[0].strip('°'),
+         boats[boat]['wind'].split(',')[1].strip().strip('kn.'))
+        for boat in list(boats.keys()) if 'lat_dec' in boats[boat]]
+
+df = pd.DataFrame(data, columns=['Boat', 'Lat', 'Lon', 'Rank', 'Track', 'Timestamp', 'HDG', 'SPD', 'TWD', 'TWS'])
+df['Rank'] = df['Rank'].astype('int64')
 df.sort_values('Rank', ascending=True, inplace=True)
 df.reset_index(inplace=True)
 
-colors=['red', 'green', 'darkblue', 'orange', 'pink', 'darkgreen',
-        'beige', 'darkred', 'purple', 'darkpurple']
+colors = ['red', 'green', 'darkblue', 'orange', 'pink', 'darkgreen',
+          'beige', 'darkred', 'purple', 'darkpurple']
 
-center=(df.loc[0, 'Lat'], df.loc[0, 'Lon'])
+center = (df.loc[0, 'Lat'], df.loc[0, 'Lon'])
 
 timestamp = df.loc[df.Boat == boat, 'Timestamp'].values[0]
 last_updated = datetime.fromtimestamp(timestamp).strftime('%d-%h %H:%M')
@@ -49,25 +51,37 @@ if len(hrs) < 2:
 if len(mins) < 2:
     mins = '0' + mins
 
-title_html = '<h3 align="center" style="font-size:16px"><b>Last updated {} ({}:{} ago)</b></h3>'.format(last_updated, hrs, mins)
+title_html = '<h3 align="center" style="font-size:16px"><b>Last updated {} ({}:{} ago)</b></h3>'.format(last_updated,
+                                                                                                        hrs, mins)
 
-mymap=folium.Map(location=center, zoom_start=7)
+mymap = folium.Map(location=center, zoom_start=7)
 mymap.get_root().html.add_child(folium.Element(title_html))
 
 for idx in df.index:
-    lat=df.loc[idx, 'Lat']
-    lon=df.loc[idx, 'Lon']
-    name=df.loc[idx, 'Boat'].upper()
-    rank=df.loc[idx, 'Rank']
+    lat = df.loc[idx, 'Lat']
+    lon = df.loc[idx, 'Lon']
+    name = df.loc[idx, 'Boat'].upper()
+    rank = df.loc[idx, 'Rank']
+    track = df.loc[idx, 'Track']
+    spd = df.loc[idx, 'SPD']
+    hdg = df.loc[idx, 'HDG']
+    twd = df.loc[idx, 'TWD']
+    tws = df.loc[idx, 'TWS']
     if idx < len(colors):
-        color=colors[idx]
+        color = colors[idx]
     else:
-        color='lightgray'
-    if df.loc[idx, 'Boat']==boat:
-        color='blue'
-    popup=str(lat) + ', ' + str(lon)
-    folium.Marker([lat, lon], popup=name + ' ' + str(rank), icon=folium.Icon(color=color, icon='sailboat', prefix='fa')).add_to(mymap)
-    folium.PolyLine(df.loc[idx, 'Track'], color=color, weight=2.5, opacity=1).add_to(mymap)
+        color = 'lightgray'
+    if df.loc[idx, 'Boat'] == boat:
+        color = 'blue'
+    popup = '{} rank: {} {} kn'.format(name, str(rank), spd)
+
+    BoatMarker([lat, lon], color=color,
+               heading=hdg,
+               wind_heading=twd,
+               wind_speed=tws,
+               popup=popup).add_to(mymap)
+    folium.PolyLine(track, color=color, weight=2.5, opacity=1).add_to(mymap)
+
 mymap.save(html)
 
 if launch_browser == '1':
