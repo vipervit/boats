22bbca4257ff1005338ba5b94c738fa9f4ec87ae
Viper Vit <vitolg1@gmail.com>
2023-01-17 12:16:58 -0500

    Improve common.py, track.py

--- lib/common.py
+++ lib/common.py
@@ -3,37 +3,50 @@ import os
 from pathlib import Path
 from datetime import datetime
 
-MY_USER='Viper Vit'
+MY_USER = 'Viper Vit'
 
-DIR_HOME=str(Path.home())
+DIR_HOME = str(Path.home())
 DIR_ONSAIL = os.path.join(DIR_HOME, 'Documents', 'OnSail')
 DIR_SAILDATA = os.path.join(DIR_ONSAIL, 'data')
 DIR_HTML = DIR_ONSAIL
 
-API_RACES = {
-    'Stardust': 'https://sarl.ingenium.net.au/racelog?racenr=38602',
-    'THE OCEAN RACE 2023 - 1 ère Etape': ''
+URL_RACES = 'https://sarl.ingenium.net.au/racelog?racenr='
+
+RACE_IDS = {
+    'Stardust': 38602,
+    'The Ocean Race': 39147
 }
 
-API_OWN_BOATS = 'http://srv.sailaway.world/cgi-bin/sailaway/APIBoatInfo.pl?usrnr=59528&key=7B79EE2988A44080A37C06570F4B5EE8'
+API_OWN_BOATS = 'http://srv.sailaway.world/cgi-bin/sailaway/APIBoatInfo.pl?usrnr=59528&key' \
+                '=7B79EE2988A44080A37C06570F4B5EE8'
 
-URL_IBOATING_CHART = 'https://fishing-app.gpsnauticalcharts.com/i-boating-fishing-web-app/fishing-marine-charts-navigation.html'
+URL_IBOATING_CHART = 'https://fishing-app.gpsnauticalcharts.com/i-boating-fishing-web-app/fishing-marine-charts' \
+                     '-navigation.html'
+
 
 def get_all_own_boats_json():
     return requests.get(API_OWN_BOATS).json()
 
-def get_race_data(race):
-    return requests.get(API_RACES[race]).json()['result']
+
+def get_race_data(race_name):
+    return requests.get(get_race_api_url(race_name)).json()['result']
+
 
 def get_boat_race_data(race, user, boat):
     return get_race_data(race)[user + '-' + boat]
 
+
+def get_race_api_url(race_name):
+    return '{}{}'.format(URL_RACES, str(RACE_IDS[race_name]))
+
+
 def timestamp():
     return datetime.now().strftime('%d/%m %H:%M')
 
-def timeago(timestamp):
+
+def timeago(time_stamp):
     now = datetime.fromtimestamp(datetime.now().timestamp())
-    delta = (now - datetime.fromtimestamp(timestamp)).seconds
-    hrs = int(delta/3600)
-    mins = int(delta/60) - hrs*60
-    return (hrs, mins)
+    delta = (now - datetime.fromtimestamp(time_stamp)).seconds
+    hrs = int(delta / 3600)
+    mins = int(delta / 60) - hrs * 60
+    return hrs, mins
--- track.py
+++ track.py
@@ -14,22 +14,22 @@ from folium.plugins import BoatMarker
 from lib.boat import boat
 from lib.common import DIR_SAILDATA, MY_USER, get_boat_race_data, DIR_HTML
 
-boatname = sys.argv[1]
+boat_name = sys.argv[1]
 race = sys.argv[2]
 launch_browser = sys.argv[3]
 
-user=MY_USER
-html=os.path.join(DIR_HTML, boatname + '.html')
+user = MY_USER
+html = os.path.join(DIR_HTML, boat_name + '.html')
 
 timestamp=datetime.now().strftime('%d-%b %H:%M')
 
-oBoat = boat(boatname)
+oBoat = boat(boat_name)
 oBoat.getdata()
-curr_pos = [round(oBoat.pos[0],3), round(oBoat.pos[1],3)]
+curr_pos = [round(oBoat.pos[0], 3), round(oBoat.pos[1], 3)]
 sog = oBoat.nav['sog']
 hdg = oBoat.nav['hdg']
 
-race_data = get_boat_race_data(race, user, boatname)
+race_data = get_boat_race_data(race, user, boat_name)
 rank = race_data['rank']
 track = race_data['track']
 track.reverse()
@@ -41,9 +41,9 @@ df_track.columns=['Lat', 'Lon']
 title_html = '<h3 align="center" style="font-size:16px">{}                  {},{}     {}°    {} kn<b></b></h3>'.\
     format(timestamp, curr_pos[0], curr_pos[1], hdg, sog)
 
-mymap=folium.Map(location=curr_pos, zoom_start=7)
+mymap = folium.Map(location=curr_pos, zoom_start=7)
 mymap.get_root().html.add_child(folium.Element(title_html))
-popup='rank: {}'.format(rank)
+popup = 'rank: {}'.format(rank)
 
 folium.PolyLine(track).add_to(mymap)
 BoatMarker(curr_pos, color='blue',
