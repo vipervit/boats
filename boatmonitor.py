efc7c92ec5fbe9bb1afe08e0f392be3bdffbe6e9
vipervit <vitolg1@gmail.com>
2024-11-03 17:59:44 -0500

    First working dead reckoning.

--- boatmonitor.py
+++ boatmonitor.py
@@ -7,13 +7,18 @@ import wx
 from geopy import distance
 
 from boats.lib.boat import Boat
-from boats.lib.common import miles_to_nautical, seconds_to_formatted_output, get_destination_coordinates
+from boats.lib.common import miles_to_nautical, seconds_to_formatted_output, get_destination_coordinates, \
+    get_estimated_position
+
 
 class BoatPopup(wx.Frame):
     polling_interval: int
 
     def __init__(self, boat_name):
         super(BoatPopup, self).__init__(parent=None, title=boat_name.upper())
+
+        self.dr_params = []
+        self.inferred = None
 
         self.boat = Boat(boat_name)
         self.counter = 0
@@ -31,49 +36,95 @@ class BoatPopup(wx.Frame):
 
         self.vbox = wx.BoxSizer(wx.VERTICAL)
 
+        # Status info
+
         self.txt_info = wx.StaticText(self)
+        self.vbox.Add(self.txt_info, 0, wx.ALIGN_LEFT)
+
+        # Polling
+
         self.txt_poll = wx.StaticText(self, label='Polling interval:')
         self.txt_zoom = wx.StaticText(self, label='Zoom level: ')
         self.edctl_zoom = wx.TextCtrl(self, value=str(self.boat.map.zoom))
         self.edctl_poll = wx.TextCtrl(self, value=str(int(self.polling_interval / 1000)))
-        self.txt_enter_dest = wx.StaticText(self, label='Enter destination:')
-        self.edctl_destination = wx.TextCtrl(self)
-        self.txt_dest_coors = wx.StaticText(self)
-        self.txt_next_upd = wx.StaticText(self)
-        self.txt_time = wx.StaticText(self)
-        self.txt_last_update = wx.StaticText(self)
-        self.btn_open_map = wx.Button(self, 0, 'Open map')
-        self.btn_update = wx.Button(self, 0, 'Update Now')
-        self.btn_exit = wx.Button(self, 0, 'Exit')
-        self.btn_enter_dest = wx.Button(self, 0, 'OK')
-        self.btn_open_map_dr = wx.Button(self, 0, 'Open DR map')
 
-        self.vbox.Add(self.txt_info, 0, wx.ALIGN_LEFT)
-        self.vbox.Add(self.txt_enter_dest, 0, wx.ALIGN_LEFT)
-        self.vbox.Add(self.edctl_destination, 0, wx.ALIGN_LEFT)
-        self.vbox.Add(self.btn_enter_dest, 0, wx.ALIGN_LEFT)
-        self.vbox.Add(self.txt_dest_coors, 0, wx.ALIGN_LEFT)
         self.vbox.Add(self.txt_poll, 0, wx.ALIGN_RIGHT)
         self.vbox.Add(self.edctl_poll, 0, wx.ALIGN_RIGHT)
         self.vbox.Add(self.txt_zoom, 0, wx.ALIGN_RIGHT)
         self.vbox.Add(self.edctl_zoom, 0, wx.ALIGN_RIGHT)
-        self.vbox.Add(self.txt_time, 0, wx.ALIGN_LEFT)
+
+        self.edctl_zoom.Bind(wx.EVT_TEXT, self.__set_zoom__)
+        self.edctl_poll.Bind(wx.EVT_TEXT, self.__set_polling_interval__)
+        self.Bind(wx.EVT_TIMER, self.__update__, self.poll_timer)
+
+        # Destination
+
+        self.txt_enter_dest = wx.StaticText(self, label='Enter destination:')
+        self.edctl_destination = wx.TextCtrl(self)
+        self.edctl_dr_lon = wx.TextCtrl(self)
+        self.txt_dest_coors = wx.StaticText(self)
+        self.btn_enter_dest = wx.Button(self, 0, 'OK')
+
+        self.vbox.Add(self.txt_enter_dest, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.edctl_destination, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.btn_enter_dest, 0, wx.ALIGN_LEFT)  # enter destination
+        self.vbox.Add(self.txt_dest_coors, 0, wx.ALIGN_LEFT)
+
+        self.btn_enter_dest.Bind(wx.EVT_BUTTON, self.__set_destination__)
+
+        # DR params
+
+        self.txt_enter_dr = wx.StaticText(self, label='Enter DR parameters\nTime period (hrs):')
+        self.edctl_dr_period = wx.TextCtrl(self)
+        self.txt_enter_last_lat = wx.StaticText(self, label='Last position lat:')
+        self.edctl_dr_last_lat = wx.TextCtrl(self)
+        self.txt_enter_last_lon = wx.StaticText(self, label='Last position lon:')
+        self.edctl_dr_last_lon = wx.TextCtrl(self)
+        self.btn_enter_dr = wx.Button(self, 0, 'OK')
+        self.txt_inferred_display = wx.StaticText(self, label='Inferred position:')
+        self.txt_inferred = wx.StaticText(self)
+
+        self.vbox.Add(self.txt_enter_dr, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.edctl_dr_period, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.txt_enter_last_lat, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.edctl_dr_last_lat, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.txt_enter_last_lon, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.edctl_dr_last_lon, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.btn_enter_dr, 0, wx.ALIGN_LEFT)  # enter DR params
+        self.vbox.Add(self.txt_inferred_display, 0, wx.ALIGN_LEFT)
+        self.vbox.Add(self.txt_inferred, 0, wx.ALIGN_LEFT)
+
+        self.btn_enter_dr.Bind(wx.EVT_BUTTON, self.__calculate_dr__)
+
+        # Updates info
+
+        self.txt_next_upd = wx.StaticText(self)
+        self.txt_time = wx.StaticText(self)
+        self.txt_last_update = wx.StaticText(self)
+
         self.vbox.Add(self.txt_last_update, 0, wx.ALIGN_LEFT)
         self.vbox.Add(self.txt_next_upd, 0, wx.ALIGN_LEFT)
+
+        # Buttons
+
+        self.btn_open_map = wx.Button(self, 0, 'Open map')
+        self.btn_update = wx.Button(self, 0, 'Update Now')
+        self.btn_exit = wx.Button(self, 0, 'Exit')
+
+        self.btn_open_map_dr = wx.Button(self, 0, 'Open DR map')
+
+        self.vbox.Add(self.txt_time, 0, wx.ALIGN_LEFT)
         self.vbox.Add(self.btn_open_map, -2, wx.ALIGN_CENTER)  # Open map
         self.vbox.Add(self.btn_open_map_dr, -2, wx.ALIGN_CENTER)  # Open DR map
         self.vbox.Add(self.btn_update, -2, wx.ALIGN_CENTER)  # Update Now
         self.vbox.Add(self.btn_exit, -1, wx.ALIGN_CENTER)  # Exit
 
-        self.Bind(wx.EVT_TIMER, self.__update__, self.poll_timer)
-        self.edctl_zoom.Bind(wx.EVT_TEXT, self.__set_zoom__)
-        self.edctl_poll.Bind(wx.EVT_TEXT, self.__set_polling_interval__)
         self.btn_exit.Bind(wx.EVT_BUTTON, self.__close__)
         self.btn_update.Bind(wx.EVT_BUTTON, self.__update__)
-        self.btn_open_map.Bind(wx.EVT_BUTTON, self.__open_map_known__)
+
+        self.btn_open_map.Bind(wx.EVT_BUTTON, self.__open_map_known__) # open map with actual known position
+        self.btn_open_map_dr.Bind(wx.EVT_BUTTON, self.__open_map_dr__) # open map with inferred (DR) position
         self.Bind(wx.EVT_TIMER, self.__update_display__, self.heartbeat_timer)
-        self.btn_enter_dest.Bind(wx.EVT_BUTTON, self.__set_destination__)
-        self.btn_open_map_dr.Bind(wx.EVT_BUTTON, self.__open_map_dr__)
 
         self.SetSizer(self.vbox)
 
@@ -125,16 +176,31 @@ class BoatPopup(wx.Frame):
             self.boat.get_data()
         self.boat.map.show(self.__get_last_update_timestamp__())
 
-    def __get_dead_reckoning_params__(self, event):
-        pass
-
-    @staticmethod
-    def __calculate_position__():
-        pos=(48.67, -2.31)
-        return pos
-
     def __open_map_dr__(self, event):
-        self.boat.map.show_calculated_position(self.__calculate_position__())
+        self.boat.map.show_calculated_position(self.inferred)
+
+    def __calculate_dr__(self, event):
+        self.__get_dead_reckoning_params__()
+        self.__infer_position__()
+
+    def __get_dead_reckoning_params__(self):
+        lat = self.edctl_dr_last_lat.GetLineText(0)
+        lon = self.edctl_dr_last_lon.GetLineText(0)
+        period = self.edctl_dr_period.GetLineText(0)
+        self.dr_params.append(float(period))
+        self.dr_params.append((float(lat), float(lon)))
+
+    def __infer_position__(self):
+        period = self.dr_params[0]
+        last_pos = self.dr_params[1]
+        df = self.boat.get_logged_data()
+        delta = timedelta(hours=period)
+        cutoff_idx = [x for x in df.index if x >= df.index[-1] - delta][0]
+        dfdr = df[df.index >= cutoff_idx]
+        mean_spd = dfdr['spd'].mean()
+        mean_hdg = dfdr['hdg'].mean()
+        self.inferred = get_estimated_position(last_pos, mean_hdg, mean_spd, elapsed=period)
+        self.txt_inferred.SetLabel(f'({self.inferred[0]}, {self.inferred[1]})')
 
     def __update_txt_info__(self):
         df = self.boat.get_logged_data()
@@ -159,7 +225,7 @@ class BoatPopup(wx.Frame):
         self.txt_info.SetLabel(text)
 
     def __update__(self, event):
-        # self.boat.get_data()
+        self.boat.get_data()
         self.__update_txt_info__()
 
     @staticmethod
