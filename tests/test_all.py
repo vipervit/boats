9402c6edf87d8aed8ea6d78a82edc9ec8027fda4
vipervit <vitolg1@gmail.com>
2025-01-10 19:22:40 -0500

    Modify tests: separate test boats from prod by name, remove some tests.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,6 @@
 import os
 import time
+import warnings
 
 import pytest
 
@@ -11,78 +12,77 @@ from boats.lib.log import Log
 from boats.lib.map import Map, MapMarker
 from boats.lib.nav import Nav
 
-TEST_BOAT_NAME = 'Testboat'
+TEST_BOAT_NAME_LOCAL = 'Testboat_Local'
+TEST_BOAT_NAME_REMOTE = 'Testboat_Remote'
 
 
 class Test_Boat:
+
     @pytest.fixture
-    def boat(self):
-        return Boat(name=TEST_BOAT_NAME, getdata=False)
+    def boatmarker(self):
+        return MapMarker(location=[0, 0],
+                         heading=0,
+                         wind_heading=0,
+                         wind_speed=0
+                         )
 
-    def test_savetolog_is_false_by_default(self, boat):
-        assert boat.nav.savetolog is False
-
-    def test_getdata_is_false(self, boat):
-        assert bool(boat.nav._data) is False  # dictionary is empty
-        with pytest.raises(AttributeError):
-            boat.nav.show()
-
-    def test_nav_default_datasource_set_to_remote(self, boat):
-        assert boat.nav.datasource == datasource.remote
-
-    def test_cannot_save_to_log_if_local(self, boat):
-        boat.nav.savetolog = True
-        with pytest.raises(ValueError):
-            boat.update_from_log()
-
-    def test_update_local(self, boat):
+    @pytest.fixture
+    def boat_local(self, boatmarker):
+        boat = Boat(name=TEST_BOAT_NAME_LOCAL, getdata=False)
         boat.update_from_log()
-        rec = boat.log.last_record
+        yield boat
+        boat.map.__delete_folium_html__()
+
+    @pytest.fixture
+    def boat_remote(self, boatmarker):
+        boat = Boat(name=TEST_BOAT_NAME_REMOTE)
+        yield boat
+        boat.map.__delete_folium_html__()
+
+    def test_savetolog_is_false_by_default(self, boat_local):
+        assert boat_local.nav.savetolog is False
+
+    def test_nav_default_datasource_set_to_remote(self, boat_remote):
+        assert boat_remote.nav.datasource == datasource.remote
+
+    def test_cannot_save_to_log_if_local(self, boat_local):
+        boat_local.nav.savetolog = True
+        with pytest.raises(ValueError):
+            boat_local.update_from_log()
+
+    def test_update_local(self, boat_local):
+        boat_local.update_from_log()
+        rec = boat_local.log.last_record
         assert rec['lat'] == 39.5066
         assert rec['lon'] == -31.41832
 
-    def test_update_remote(self, boat):
-        boat.update_from_server()
-        nav = boat.nav
+    def test_update_remote(self, boat_remote):
+        boat_remote.update_from_server()
+        nav = boat_remote.nav
         assert nav.heel == 17
         assert nav.speed['spd'] == 4.6
         assert nav.az['hdg'] == 104
         assert nav.sailplan == ['Mainsail', 'Genaker']
 
-    def test_can_access_log(self, boat):
-        assert boat.log.last_record_timestamp == '22-Oct 06:45'
-
-    def test_no_nav_until_updated(self, boat):
-        assert bool(boat.nav.all) == False  # empty
-        with pytest.raises(AttributeError):
-            assert boat.nav.show()
-
-    # def test_show_map_updated_from_log(self):
-    #     boat = Boat(name=TEST_BOAT_NAME, getdata=False)
-    #     boat.update_from_log()
-    #     boat.map.show()
-    #
-    # def test_show_map_updated_from_server(self):
-    #     boat = Boat(name='Petsamo')
-    #     boat.map.show()
-
+    def test_can_access_log(self, boat_local):
+        assert boat_local.log.last_record_timestamp == '22-Oct 06:45'
 
 class Test_Nav:
     @pytest.fixture
     def nav_remote(self):
-        return Nav(boat_name=TEST_BOAT_NAME, src=datasource.remote)
+        return Nav(boat_name=TEST_BOAT_NAME_REMOTE, src=datasource.remote)
 
     @pytest.fixture
     def nav_local(self):
-        return Nav(boat_name=TEST_BOAT_NAME, src=datasource.local)
+        return Nav(boat_name=TEST_BOAT_NAME_LOCAL, src=datasource.local)
 
     def test_get_data(self, nav_local, nav_remote):
         # local
-        assert nav_local.boatname == TEST_BOAT_NAME
+        assert nav_local.boatname == TEST_BOAT_NAME_LOCAL
         assert nav_local.heel == 35
         assert nav_local.sailplan == ['Mainsail', 'Nr.2']
         # remote
-        assert nav_remote.boatname == TEST_BOAT_NAME
+        assert nav_remote.boatname == TEST_BOAT_NAME_REMOTE
         assert nav_remote.heel == 17
         assert nav_remote.speed['spd'] == 4.6
         assert nav_remote.az['hdg'] == 104
@@ -98,7 +98,7 @@ class Test_Nav:
 class Test_Log:
     @pytest.fixture()
     def boatlog(self):
-        return Log(boat_name=TEST_BOAT_NAME)
+        return Log(boat_name=TEST_BOAT_NAME_LOCAL)
 
     @pytest.fixture()
     def nonexistinglog(self):
@@ -146,7 +146,7 @@ class Test_Map:
     @pytest.fixture()
     def mymap(self):
         loc = [43.632, -79.387]
-        return Map(boat_name=TEST_BOAT_NAME,
+        return Map(boat_name=TEST_BOAT_NAME_LOCAL,
                    title='TORONTO HARBOUR',
                    location=loc,
                    zoom_start=15,
@@ -175,5 +175,3 @@ class Test_Map:
     def test_delete_folium(self, mymap):
         mymap.__delete_folium_html__()
         assert not os.path.exists(mymap._mfile)
-
-
