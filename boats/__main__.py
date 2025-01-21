57a0d35207e858541a48d74e6d7e136bee290451
vipervit <vitolg1@gmail.com>
2025-01-21 16:54:49 -0500

    Modify imports.

--- boats/__main__.py
+++ boats/__main__.py
@@ -1,12 +1,8 @@
 import argparse
-import os
-import shutil
 import sys
 
-from boats import boatmonitor, DIR_LOGS
+from boats import boatmonitor
 from boats import viewboat as viewboat
-from boats.lib.boat import Boat
-from boats.lib.common import get_version_from_pyproject, make_log_file_name
 from boats.lib.dest import Destinations
 
 
@@ -23,28 +19,6 @@ def view_destinations():
     Destinations().view()
 
 
-def copy_log(boatname, outdir, action):
-    src, dest = None, None
-    fname = make_log_file_name(boatname)
-    inloc = os.path.join(DIR_LOGS, fname)
-    outloc = os.path.join(outdir, fname)
-    match action:
-        case 'install':
-            src = outloc
-            dest = inloc
-        case 'backup':
-            src = inloc
-            dest = outloc
-    shutil.copy(src, dest)
-
-
-def view_log_last(boatname):
-    with Boat(boatname, getdata=False) as boat:
-        boat.update_from_log()
-        print(boat.log.last_record_timestamp)
-        print(f'{boat.log.last_record_timestamp_local} EST')
-
-
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--viewboat', action='store_true')
@@ -58,13 +32,6 @@ def main(args):
     parser.add_argument('--add', type=str)
     parser.add_argument('--remove', type=str)
     parser.add_argument('--view', action='store_true')
-    parser.add_argument('--log', action='store_true')
-    parser.add_argument('--backup', action='store_true')
-    parser.add_argument('--install', action='store_true')
-    parser.add_argument('--viewlast', action='store_true')
-    parser.add_argument('--dir', type=str)
-    parser.add_argument('--from_log', action='store_true')
-    parser.add_argument('--version', action='store_true')
 
     args = parser.parse_args(args)
 
@@ -81,16 +48,6 @@ def main(args):
             view_destinations()
         if args.view:
             view_destinations()
-    if args.log:
-        if args.install:
-            copy_log(args.boat_name, args.dir, action='install')
-        if args.backup:
-            copy_log(args.boat_name, args.dir, action='backup')
-        if args.viewlast:
-            view_log_last(args.boat_name)
-
-    if args.version:
-        print(get_version_from_pyproject())
 
 
 if __name__ == "__main__":
