d4060ae42bf5b20d58b6318231744f746dbd0898
vipervit <vitolg1@gmail.com>
2025-01-21 16:54:49 -0500

    Modify imports.

--- boats/__main__.py
+++ boats/__main__.py
@@ -1,11 +1,8 @@
 import argparse
-import os
-import shutil
 import sys
 
 from boats import boatmonitor
 from boats import viewboat as viewboat
-from boats.lib.boat import Boat
 from boats.lib.dest import Destinations
 
 
@@ -22,12 +19,6 @@ def view_destinations():
     Destinations().view()
 
 
-def copy_log(boatname, destdir):
-    logfile = Boat(boatname).log.myfile
-    dest_fname = logfile.split(os.sep)[-1]
-    shutil.copy(logfile, os.path.join(destdir, dest_fname))
-
-
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--viewboat', action='store_true')
@@ -41,9 +32,6 @@ def main(args):
     parser.add_argument('--add', type=str)
     parser.add_argument('--remove', type=str)
     parser.add_argument('--view', action='store_true')
-    parser.add_argument('--copy_log', action='store_true')
-    parser.add_argument('--todir', type=str)
-    parser.add_argument('--from_log', action='store_true')
 
     args = parser.parse_args(args)
 
@@ -60,8 +48,6 @@ def main(args):
             view_destinations()
         if args.view:
             view_destinations()
-    if args.copy_log:
-        copy_log(args.boat_name, args.todir)
 
 
 if __name__ == "__main__":
