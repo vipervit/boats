71394e51dea160731f940a56ff35632e78a7b7ef
vipervit <vitolg1@gmail.com>
2025-01-31 08:23:24 -0500

    Add TODO's. Fix setting defaults and data exchanges. Add README.md.

---
+++ boats/docs/README.md
@@ -0,0 +1,1 @@
+[Sail monitor](sailmonitor.png)
--- boats/lib/gui/mapbox.py
+++ boats/lib/gui/mapbox.py
@@ -6,22 +6,22 @@ from boats.lib.gui.box import Box
 class MapBox(Box, wx.BoxSizer):
 
     def __init__(self, mapfile, parent):
+        self._map = None
+        self._f = mapfile
         super(Box, self).__init__()
         super(MapBox, self).__init__(parent)
-        self._map = None
-        self.f = mapfile
-        self.__add_map__()
+
 
     def __create_map__(self):
         browser = wx.html2.WebView.New(self.parent)
-        browser.LoadURL(f'file:///{self.f}')
+        browser.LoadURL(f'file:///{self._f}')
         self._map = browser
 
-    def __add_map__(self):
+    def __draw_layout__(self):
         self.__create_map__()
         self.Add(self._map, 1, wx.EXPAND)
 
     def update(self):
         self.Detach(self._map)
-        self.__add_map__()
+        self.__draw_layout__()
         self.Layout()
